0a336cec0421dc8ab13c23d4094a4f6e0fc0a3c3
BenArayathel <ben.arayathel@gmail.com>
2022-11-16 16:09:10 -0500

    adding notes

--- demos/2-Introduction To Maven, Testing and Exceptions/UnitTestChallenge/src/main/java/com/revature/service/Calculator.java
+++ demos/2-Introduction To Maven, Testing and Exceptions/UnitTestChallenge/src/main/java/com/revature/service/Calculator.java
@@ -4,6 +4,16 @@ import com.revature.exception.InfinityException;
 
 public interface Calculator {
 	
+	/*
+	 * I would like you to practice some TDD for this challenge by adding some unit tests for the
+	 * following methods. 
+	 * 
+	 * It's up to you on how many test cases you want for each method, but I recommend considering
+	 * all edge cases - e.g. dividing by zero or passing in a number bigger than double can store. 
+	 * 
+	 * You don't need to consider testing for exceptions, unless you're feeling brave. 
+	 */
+	
 	
 	double add(double x, double y);
 	
