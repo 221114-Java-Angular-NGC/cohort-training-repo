3ca1dcc74e5d2d06768d5ee35c6214cf85c3743e
aaknox <azhya.knox@gmail.com>
2022-11-28 13:12:37 -0600

    added horoscope app

--- demos/2-Introduction To Maven, Testing and Exceptions/HelloJDBC/src/main/java/com/revature/models/User.java
+++ demos/2-Introduction To Maven, Testing and Exceptions/HelloJDBC/src/main/java/com/revature/models/User.java
@@ -3,6 +3,16 @@ package com.revature.models;
 import java.time.LocalDate;
 import java.util.Objects;
 
+import lombok.EqualsAndHashCode;
+import lombok.Getter;
+import lombok.Setter;
+import lombok.ToString;
+
+//Lombok allows you to have boilerplate code for your models based on annotations from this library
+@Getter
+@Setter
+@ToString
+@EqualsAndHashCode
 public class User {
 
 	private int id;
@@ -11,6 +21,7 @@ public class User {
 	private String firstName;
 	private String lastName;
 	private LocalDate birthdate;
+	
 	public User() {
 		super();
 		// TODO Auto-generated constructor stub
@@ -31,63 +42,5 @@ public class User {
 		this.firstName = firstName;
 		this.lastName = lastName;
 		this.birthdate = birthdate;
-	}
-	public int getId() {
-		return id;
-	}
-	public void setId(int id) {
-		this.id = id;
-	}
-	public String getUsername() {
-		return username;
-	}
-	public void setUsername(String username) {
-		this.username = username;
-	}
-	public String getPassword() {
-		return password;
-	}
-	public void setPassword(String password) {
-		this.password = password;
-	}
-	public String getFirstName() {
-		return firstName;
-	}
-	public void setFirstName(String firstName) {
-		this.firstName = firstName;
-	}
-	public String getLastName() {
-		return lastName;
-	}
-	public void setLastName(String lastName) {
-		this.lastName = lastName;
-	}
-	public LocalDate getBirthdate() {
-		return birthdate;
-	}
-	public void setBirthdate(LocalDate birthdate) {
-		this.birthdate = birthdate;
-	}
-	@Override
-	public int hashCode() {
-		return Objects.hash(birthdate, firstName, id, lastName, password, username);
-	}
-	@Override
-	public boolean equals(Object obj) {
-		if (this == obj)
-			return true;
-		if (obj == null)
-			return false;
-		if (getClass() != obj.getClass())
-			return false;
-		User other = (User) obj;
-		return Objects.equals(birthdate, other.birthdate) && Objects.equals(firstName, other.firstName)
-				&& id == other.id && Objects.equals(lastName, other.lastName)
-				&& Objects.equals(password, other.password) && Objects.equals(username, other.username);
-	}
-	@Override
-	public String toString() {
-		return "User [id=" + id + ", username=" + username + ", password=" + password + ", firstName=" + firstName
-				+ ", lastName=" + lastName + ", birthdate=" + birthdate + "]";
 	}
 }
