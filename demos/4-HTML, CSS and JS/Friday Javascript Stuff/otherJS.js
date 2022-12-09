ecab8e8d57706963c4112388da96b69c4d9e3e16
BenArayathel <ben.arayathel@gmail.com>
2022-12-09 16:20:20 -0500

    adding more notes for Friday

--- demos/4-HTML, CSS and JS/Friday Javascript Stuff/otherJS.js
+++ demos/4-HTML, CSS and JS/Friday Javascript Stuff/otherJS.js
@@ -45,8 +45,122 @@ function variableArguments(...input){
     }
 }
 
-variableArguments(1,2,3,4);
-variableArguments([1,2,3]);
-variableArguments(1,2,3);
-variableArguments(arr);
-variableArguments(...arr);
+// variableArguments(1,2,3,4);
+// variableArguments([1,2,3]);
+// variableArguments(1,2,3);
+// variableArguments(arr);
+// variableArguments(...arr);
+
+
+//-----------Iteratos, Iterables and Generators-------------------
+
+//Iterables
+// Objects that have an iterators in them. 
+// This allows them to be leveraged by other function, most notably the "for of"
+
+//Iterator: 
+// An iterator is an object that defines a sequence for us. 
+// A iterator object has a "next()" method, which will either return the next value in the sequence 
+// or it will return "finsh" status
+
+//An example is an array 
+let newArray = [27,81,85,23];
+
+//arrays can be used with a for of loop
+// for(let i of newArray){
+//     console.log(i);
+// }
+
+//the next method will return 2 values, the value of the next sequence and whether it is "done".
+let iterator = newArray[Symbol.iterator]();
+// console.log(iterator);
+// console.log(iterator.next());
+// console.log(iterator.next());
+// console.log(iterator.next());
+// console.log(iterator.next());
+// console.log(iterator.next());
+
+
+// let looping = true;
+// while(looping){
+
+//     looping = iterator.next().done;
+// }
+
+
+//Making our own iterator
+
+function createIterator(start=0,end=10,step=1){
+    // let start = 0; //starting condtion
+    // let end = 10; // ending condition
+    // let step = 2; // incrementing 1 step at a time
+
+    let nextIndex = start;
+
+    const rangeIterator = {
+
+        next(){
+            let result;
+            if( nextIndex < end){ //there are still more elements
+                result = {value: nextIndex, done: false}
+                nextIndex += step; //increment by the step value
+                return result;
+            }else{
+                return {value:end, done: true}
+            }
+        }
+    }
+
+    return rangeIterator;
+}
+
+let myOwnIterator = createIterator(-100,100,7);
+
+// let looping = true;
+// while(looping){
+
+//     let result = myOwnIterator.next();
+
+//     console.log(result.value);
+
+//     if(result.done == true){
+//         looping = false;
+//     }
+    
+// }
+
+//Generator Function: 
+// This is an alternative to creating an iterator.
+// We can define a special function to create and return a generator, the generator will keep 
+// sequencing until it stop at the yield we have provided.  
+
+function* makeGeneratorIterator(start=0,end=10,step=1){
+    let count = 0;
+    for(let i = start; i<end; i+=step){
+        count++;
+        yield i;
+    }
+    return count;
+}
+
+let myGenerator = makeGeneratorIterator(-10,20,2);
+let result = myGenerator.next();
+console.log(result);
+
+while(result.value < 20){
+    console.log(result.value);
+    result = myGenerator.next();
+}
+
+// let looping = true;
+// while(looping){
+
+//     let result = makeGeneratorIterator.next();
+
+//     console.log(result.value);
+
+//     if(result.done == true){
+//         looping = false;
+//     }
+    
+// }
